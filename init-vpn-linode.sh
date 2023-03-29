b3a2ade13402f306cb058dd8b1dc16648fd48844
ziv-buyme <ziv.g@buyme.co.il>
2023-03-29 16:33:58 +0300

    Renamed init script Added init-vpn-linode.sh for linde StackScript

---
+++ init-vpn-linode.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# <UDF name="UP_VAULT_ADDR" label="" />
+# <UDF name="VAULT_ADDR" label="" />
+# <UDF name="VAULT_TOKEN" label=""  />
+# <UDF name="ENVIRONMENT" label=""  />
+# <UDF name="INSTANCE_REGION" label=""  />
+
+apt-get update
