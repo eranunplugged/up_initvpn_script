532c18d6d13224e05896c0f5982b4daefc7a7af3
ziv-gabel <ziv@unplugged.com>
2023-05-22 15:18:42 +0300

    Reality script

--- install_reality.sh
+++ install_reality.sh
@@ -15,11 +15,38 @@ root hard     nproc          655350
 root soft     nofile         655350
 root hard     nofile         655350
 EOF
-mkdir /tmp/xray
-cd /tmp/xray
+
+cat << EOF > /etc/systemd/system/xray.service
+[Unit]
+Description=XTLS Xray-Core a VMESS/VLESS Server
+After=network.target nss-lookup.target
+[Service]
+# Change to your username <---
+User=USERNAME
+Group=USERNAME
+CapabilityBoundingSet=CAP_NET_ADMIN CAP_NET_BIND_SERVICE
+AmbientCapabilities=CAP_NET_ADMIN CAP_NET_BIND_SERVICE
+NoNewPrivileges=true
+ExecStart=/opt/xray/xray run -config /opt/xray/config.json
+Restart=on-failure
+RestartPreventExitStatus=23
+StandardOutput=journal
+LimitNPROC=100000
+LimitNOFILE=1000000
+[Install]
+WantedBy=multi-user.target
+EOF
+
+
+mkdir /opt/xray
+cd /opt/xray
 sudo apt-get update
 sudo apt-get install unzip
-curl -o xray.zip https://github.com/XTLS/Xray-core/releases/download/v1.8.1/Xray-linux-64.zip
-unzip xray.zip
-rm -f xray.zip
-./xray uuid -i Secret
+wget https://github.com/XTLS/Xray-core/releases/download/v1.8.1/Xray-linux-64.zip
+unzip Xray-linux-64.zip
+rm -f Xray-linux-64.zip
+XRAY_UUID=$(./xray uuid -i Secret)
+X25519=$(./xray x25519)
+X_PRIVATE_KEY=$(echo "$X25519" | cut -d " " -f 3)
+X_PUBLIC_KEY=$(echo "$X25519" | cut -d " " -f 5)
+
