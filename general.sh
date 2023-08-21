699760abae6b720efd390d906e520cbb035d9a70
ziv-gabel <ziv@unplugged.com>
2023-08-21 09:29:05 +0300

    Changes to support the OTHER cloud

--- general.sh
+++ general.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 {
-  export VAULT_TOKEN=ChangeME
-  export VAULT_ADDR=https://vault-dev.unpluggesystems.app
-  export UP_VAULT_ADDR=https://vault-dev.unpluggesystems.app
-  export ENVIRONMENT=ChangeME
-  export INSTANCE_CLOUD=OTHER
-  export INSTANCE_REGION=ChangeME
-  export VPN_TYPES=WIREGUARD,REALITY
-  export BRANCH=DVO-54
-  export REALITY_EXTERNAL_HOST=www.googleanalytics.com
-  export REALITY_EXTERNAL_PORT=443
-  export INSTANCE_ID=ChangeME
-  env
+export VAULT_TOKEN=
+export VAULT_ADDR=https://vault-dev.unpluggedsystems.app
+export UP_VAULT_ADDR=https://vault-dev.unpluggedsystems.app
+export ENVIRONMENT=stage
+export INSTANCE_CLOUD=OTHER
+export INSTANCE_REGION=eu-central
+export VPN_TYPES=WIREGUARD,REALITY
+export BRANCH=DVO-54
+export REALITY_EXTERNAL_HOST=www.googleanalytics.com
+export REALITY_EXTERNAL_PORT=443
+export INSTANCE_ID=hetzner_helsinki
+env
 
-  curl -o init.sh https://raw.githubusercontent.com/eranunplugged/up_initvpn_script/${BRANCH}/init-vpn-linode.sh
-  chmod 777 init.sh
-  ./init.sh
+curl -o init.sh https://raw.githubusercontent.com/eranunplugged/up_initvpn_script/${BRANCH}/init-vpn-linode.sh
+chmod 777 init.sh
+./init.sh
 } > /var/log/unplugged-vpn-script.log 2>&1 &
