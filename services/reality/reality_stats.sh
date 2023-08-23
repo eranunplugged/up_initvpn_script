3a657ebaeb3a63411a55c230ede30c89836951f1
ziv-gabel <ziv@unplugged.com>
2023-08-23 17:57:33 +0300

    Changed api to default port 8080 Added a tag so we can get statistics.

---
+++ services/reality/reality_stats.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+XRAY=/opt/xray/xray
+
+[ -x $XRAY ] || exit 1
+DL=$($XRAY api stats -name "inbound>>>reality>>>traffic>>>downlink" | jq -r .stat.value)
+UP=$($XRAY api stats -name "inbound>>>reality>>>traffic>>>uplink" | jq -r .stat.value)
+
+OUT=$(echo "{}" | jq --arg dl $DL --arg up $UP --arg hostname $HOSTNAME '. + {reality_downlink: $dl, reality_uplink: $up, host: $hostname}')
+logger "$OUT"
