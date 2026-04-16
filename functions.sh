4385aa61dfe3364ce78912716cdc43abe5b91376
AlekseiUnplugged <aleksei@unplugged.com>
2026-04-16 15:45:10 +0300

    UNP-7635 - Updated install_docker function in function.sh - apt-key is deprecated

--- functions.sh
+++ functions.sh
@@ -1,14 +1,17 @@
 function install_docker {
-  curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
-  add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+  mkdir -p /etc/apt/keyrings
+  curl -fsSL https://download.docker.com/linux/ubuntu/gpg | gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+  chmod a+r /etc/apt/keyrings/docker.gpg
+  echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+    $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | tee /etc/apt/sources.list.d/docker.list > /dev/null
   apt-get update
-  apt-get install -y docker-ce docker-ce-cli containerd.io docker-compose
+  apt-get install -y docker-ce docker-ce-cli containerd.io docker-compose-plugin
   systemctl enable --now docker
 }
 
 function install_vault() {
   export VAULT_VERSION="1.9.3" # Replace with the desired version
-  docker run -d -t --name=vault vault:${VAULT_VERSION}
+  docker run -d -t --name=vault vault:${VAULT_VERSION}so is
   docker cp vault:/bin/vault /bin/vault
   docker rm -f vault
 }
